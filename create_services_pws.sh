52b37099c7b8c30d54215da9a97da3382cd7f81e
Younjin Jeong <younjin.jeong@gmail.com>
2017-01-28 06:07:03 +0900

    change service and domain name

--- create_services_pws.sh
+++ create_services_pws.sh
@@ -1,7 +1,7 @@
-cf cups emc-forum-service-registry -p '{"uri":"http://emc-forum-eureka.cfapps.io"}'
-cf cups emc-forum-config-server -p '{"uri":"http://emc-forum-config.cfapps.io"}'
-cf cs cloudamqp lemur emc-forum-rabbitmq
-cf cs cleardb spark emc-forum-answer-db
+cf cups pivotal-meetup-apj-service-registry -p '{"uri":"http://pivotal-meetup-apj-eureka.cfapps.io"}'
+cf cups pivotal-meetup-apj-config-server -p '{"uri":"http://pivotal-meetup-apj-config.cfapps.io"}'
+cf cs cloudamqp lemur pivotal-meetup-apj-rabbitmq
+cf cs cleardb spark pivotal-meetup-apj-answer-db
 
-cf map-route emc-forum-repository cfapps.io --hostname emc-forum --path userAnswers
+cf map-route pivotal-meetup-apj-repository cfapps.io --hostname pivotal-meetup-apj --path userAnswers
 
